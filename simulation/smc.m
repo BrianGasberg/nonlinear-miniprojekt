4c4dffebe55a589b970bdb5e8fe3ff10c4aa63cd
Jens Nielsen <nens90@gmail.com>
2015-12-14 12:02:52 +0100

    ..

--- simulation/smc.m
+++ simulation/smc.m
@@ -6,7 +6,7 @@ l = 0.305;
 g = -9.81;
 Km = 0.0934;
 A = 500;
-a1 = 8;
+a1 = 10;
 
 dt = 1/200;
 
@@ -24,15 +24,16 @@ b_p = [Kf_p(1)/m_p(1) Kf_p(2)/m_p(1) Kf_p(2)/m_p(2) Kf_p(1)/m_p(2)];
 
 b_hat = mean(b_p);
 
-m = mean(m_p);
+m = max(m_p);
 Kf = mean(Kf_p);
-alpha = mean(alpha_p);
+alpha = min(alpha_p);
 
-rho1 = [abs(-a/max(c_p) - a/c_hat) abs((a1-max(b_p))/min(c_p) - (a1-b_hat)/c_hat)]
+% rho1 = [abs(-a/max(c_p) - a/c_hat) abs((a1-max(b_p))/min(c_p) - (a1-b_hat)/c_hat)]
+rho1 = [abs( (1/c_hat - 1/max(c_p))*a ) abs( (a1-min(b_p))/min(c_p) + (b_hat - a1)/c_hat )]
 rho2 = (Km*A*(5/2000)/(min(m_p)*l))/min(c_p)
 
 x2_bound = 0.01;
-theta = 0.94;
+theta = 0.93;
 
 e = (x2_bound)/norm(1-1/theta)
 
@@ -44,5 +45,5 @@ Rho_wo_dis = rho1(1)*omega_e(1);
 beta_0_wo_dis = ceil(Rho_wo_dis) - Rho_wo_dis
 beta_wo_dis = beta_0_wo_dis + Rho_wo_dis
 
-Rho = rho1*[sin(omega_e(1)); omega_e(2)] + rho2
+Rho = rho1*[omega_e(1); omega_e(2)] + rho2
 beta0 = ceil(Rho) - Rho
